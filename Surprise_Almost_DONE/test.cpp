af2ed754ba302a8c0fb68feb1ffbc8f522276366
suulcoder <ef.proyectomundial@gmail.com>
2019-09-23 11:27:35 -0600

    Still working in the mesh of bot programs

---
+++ Surprise_Almost_DONE/test.cpp
@@ -0,0 +1,26 @@
+#include <iostream> //cout, cin, cerr
+#include <unistd.h>
+#include <cmath>
+#include <fstream> //file processingy
+#include <cstdlib> //exit function
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+using namespace std;
+#define byte uint8_t
+#include<bits/stdc++.h>  
+
+int main(int argc, char const *argv[])
+{
+	int chain;
+	chain = int('h');
+	printf("h");
+	printf("%d\n",chain);
+	string ch = string(chain);
+	printf(ch);
+}
+	
+
+ 
